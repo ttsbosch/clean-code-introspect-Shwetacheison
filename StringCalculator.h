6f18808d0f576c9f2679dbee8d43c7eb61bf653b
Shwetacheison <42909769+Shwetacheison@users.noreply.github.com>
2024-06-27 15:49:22 +0530

    Update StringCalculator.h to remove complexity

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,10 +1,15 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdexcept>
 
-using namespace std;
-class StringCalculator
-{
-    public:
-    int add(string numbers);
+class StringCalculator {
+private:
+    int parseAndSum(const std::string& numbers, std::vector<int>& negatives);
+    void collectNegatives(int number, std::vector<int>& negatives);
+    void throwErrorIfNegatives(const std::vector<int>& negatives);
+
+public:
+    int add(const std::string& numbers);
 };
 
