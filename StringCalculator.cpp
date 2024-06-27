b936dbf8f69b116f5d850ee5f68052f022f09efe
Shwetacheison <42909769+Shwetacheison@users.noreply.github.com>
2024-06-27 15:22:04 +0530

    Update for testcase3 StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,8 +1,16 @@
 #include "StringCalculator.h"
+#include <sstream>
 int StringCalculator::add(string numbers){
-if(numbers=="")
-{
-return 0;
-}
-return 0;
+if (numbers=="") {
+            return 0;
+        }
+
+        istringstream stream(numbers);
+        string token;
+        int sum = 0;
+        while (getline(stream, token, ',')) {
+            sum += stoi(token);
+        }
+
+        return sum;
 }
