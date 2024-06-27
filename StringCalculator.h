8720068d5d014c75c83c1cd105b1d93507a3bdf3
Shwetacheison <42909769+Shwetacheison@users.noreply.github.com>
2024-06-27 16:07:59 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,16 +1,20 @@
-#include <iostream>
+#ifndef STRINGCALCULATOR_H
+#define STRINGCALCULATOR_H
+
+#include <sstream>
 #include <string>
 #include <vector>
-#include <sstream>
-#include <stdexcept>
 
-class StringCalculator {
-private:
+class StringCalculator
+{
+  public:
+    int add(const std::string& numbers);
+
+  private:
+    std::vector<int> parseNumbers(const std::string& numbers);
     int parseAndSum(const std::string& numbers, std::vector<int>& negatives);
     void collectNegatives(int number, std::vector<int>& negatives);
     void throwErrorIfNegatives(const std::vector<int>& negatives);
-
-public:
-    int add(const std::string& numbers);
 };
 
+#endif // STRINGCALCULATOR_H
